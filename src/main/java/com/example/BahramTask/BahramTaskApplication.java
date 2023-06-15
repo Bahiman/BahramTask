2fe7ffd480d64fd282d1778684abceaedabe681a
Bahiman <99581000+Bahiman@users.noreply.github.com>
2023-06-15 20:08:41 +0400

    Initial commit

---
+++ src/main/java/com/example/BahramTask/BahramTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.BahramTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BahramTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BahramTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BahramTask/BahramTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BahramTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BahramTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
